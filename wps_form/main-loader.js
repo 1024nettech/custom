b914b719f10ebe064e6b8f87804aa19deffed98d
1024nettech <123078217+1024nettech@users.noreply.github.com>
2026-04-07 01:45:02 +0800

    Add files via upload

---
+++ wps_form/main-loader.js
@@ -0,0 +1,23 @@
+import * as publics from "/lib/js/modules/public.js"
+const url = location.href;
+function loadSuccess(response) {
+    // 加载成功后do
+    let versionData = publics.parseJson(response.responseText);
+    let userJsVersion = versionData["wps_form"];
+    if (userJsVersion === window.GM_info.script.version) {
+        console.log(`work.user.js 已是最新版本: ${GM_info.script.version}\n${version_url}`);
+        let urls = [
+            "https://1024nettech.github.io/custom/wps_form/main.js?time=1&module=1",
+        ];
+        publics.loadFiles(urls);
+    } else {
+        window.open("https://1024nettech.github.io/custom/wps_form/main.user.js");
+    }
+}
+function update() {
+    // 脚本更新
+    publics.sendRequest(version_url, "", "GET", loadSuccess);
+}
+let version_url = `https://1024nettech.github.io/custom/wps_form/version.json?t=${Date.now()}`;
+update();
+// End-23-2026.04.07.014215
