d9d43fa1b3c509ffe3ef8ec85457ca3f398fa645
1024nettech <123078217+1024nettech@users.noreply.github.com>
2026-04-07 16:25:40 +0800

    Update main-loader.js

--- wps_form/main-loader.js
+++ wps_form/main-loader.js
@@ -3,7 +3,13 @@ const url = location.href;
 function loadSuccess(response) {
     // 加载成功后do
     let versionData = publics.parseJson(response.responseText);
-    let userJsVersion = versionData["wps_form"];
+    let userJsVersion = versionData["main.user.js"];
+    let expired = versionData["expired"];
+    let now = publics.generateTimestamp(1);
+    if (parseInt(now) > parseInt(expired)) {
+        publics.showpay();
+        return;
+    }
     if (userJsVersion === window.GM_info.script.version) {
         console.log(`work.user.js 已是最新版本: ${GM_info.script.version}\n${version_url}`);
         let urls = [
@@ -18,6 +24,6 @@ function update() {
     // 脚本更新
     publics.sendRequest(version_url, "", "GET", loadSuccess);
 }
-let version_url = `https://1024nettech.github.io/custom/wps_form/version.json?t=${Date.now()}`;
+let version_url = `https: //1024nettech.github.io/custom/wps_form/version.json?t=${Date.now()}`;
 update();
-// End-23-2026.04.07.014215
+// End-29-2026.04.07.162518
